88cf2893e086aab2abb133865e24197df72871d2
Anindita <reachanip@gmail.com>
2023-12-21 08:58:42 -0800

    submit get category by id

--- src/main/java/com/eshop/product/controller/CategoryController.java
+++ src/main/java/com/eshop/product/controller/CategoryController.java
@@ -2,8 +2,10 @@ package com.eshop.product.controller;
 
 import com.eshop.product.dto.CategoryDTO;
 import com.eshop.product.service.CategoryService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,6 +23,12 @@ public class CategoryController {
     @ResponseStatus(HttpStatus.CREATED)
     public CategoryDTO createCategory(@RequestBody CategoryDTO categoryDTO) {
         return categoryService.saveCategory(categoryDTO);
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity <CategoryDTO> getCategoryById(@PathVariable Long categoryId) throws NotFoundException {
+        CategoryDTO categoryDTO = categoryService.getCategoryById(categoryId);
+        return ResponseEntity.ok(categoryDTO);
     }
 
     // Additional endpoints as needed
--- src/main/java/com/eshop/product/service/CategoryService.java
+++ src/main/java/com/eshop/product/service/CategoryService.java
@@ -3,10 +3,12 @@ package com.eshop.product.service;
 import com.eshop.product.dto.CategoryDTO;
 import com.eshop.product.entity.Category;
 import com.eshop.product.repository.CategoryRepository;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -26,5 +28,23 @@ public class CategoryService {
 
         Category savedCategory = categoryRepository.save(category);
         return new CategoryDTO(savedCategory.getId(), savedCategory.getName(), savedCategory.getDescription());
+    }
+
+    private CategoryDTO convertToDto(Category category) {
+        return new CategoryDTO(
+                category.getId(),
+                category.getName(),
+                category.getDescription()
+                        != null ? category.getDescription() : null);
+    }
+
+    @Transactional
+    public CategoryDTO getCategoryById(Long categoryId) throws NotFoundException {
+        Optional <Category> category =  categoryRepository.findById(categoryId);
+        if(category.isPresent()){
+            return convertToDto(category.get());
+        } else {
+            throw new NotFoundException("Category not found with ID: "+ categoryId);
+        }
     }
 }
